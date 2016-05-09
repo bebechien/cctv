59ab4809dceb6ce3f4cc29422116049760c06760
bebechien <bebechien@gmail.com>
2016-05-09 13:38:57 +0900

    Update convert.sh
    오동작 수정

--- convert.sh
+++ convert.sh
@@ -3,7 +3,7 @@
 if [ $# != 1 ]; then
   echo "usage: $0 input_folder"
 else
-  mkdir re_$1
+  mkdir re_frames_$1
   CAM_IDX=1
   while [ $CAM_IDX -lt 4 ]; do
     mkdir frames_$1
@@ -19,11 +19,11 @@ else
     convert -loop 0 temp/*.png $1_$CAM_IDX.gif
     rm -rf temp
 
-    files_in_dirs=($1/cam$CAM_IDX.*)
+    files_in_dirs=(frames_$1/cam$CAM_IDX.*)
     COUNTER=0
     for item in ${files_in_dirs[*]}
     do
-      cp $item re_$1/cam$CAM_IDX.$COUNTER.png
+      cp $item re_frames_$1/cam$CAM_IDX.$COUNTER.png
       let COUNTER=COUNTER+1
     done
     rm -rf frames_$1
